8ce41306c6a17a5a9679e040f973dd7db6c7b321
adryst007 <20233L001079@utcv.edu.mx>
2025-01-23 08:15:53 -0600

    feat: enlazar vista con controlador

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -8,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  email:string="adri.cruz@gmail.com";
+  password:string="";
   constructor() { }
 
   ngOnInit() {
   }
+  login(){
+   //si usuario = admin y password = admin
+   //entonces console.log(login correcto)
+   //sinp login incorrectyo
 
+   if(this.email== 'admin' && this.password=='admin'){
+    console.log('Login correcto')
+   }else{
+    console.log('Login incorrecto');
+   }
+    
+
+  
+}
 }
