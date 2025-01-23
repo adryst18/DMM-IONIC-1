4b93d211b2bacb5c146ea5a9fe0960e9ac363969
adryst007 <20233L001079@utcv.edu.mx>
2025-01-23 09:00:43 -0600

    feat: implementar formularios

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,18 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  email:string="adri.cruz@gmail.com";
+  email:string="admin";
   password:string="";
   constructor() { }
 
   ngOnInit() {
   }
-  login(){
+  login(formulario:NgForm){
+    //console.log('email:',this.email);
+    //console.log('password: ', this.password);
+
+
+    //valores del formulario
+    console.log(formulario.value);
+
+
+    //validar el formulario
+    console.log('valid', formulario.valid);
+
+    //si form es invalido
+    //console log (todos los campos son requeridos)
+    //return
+
+    if(formulario.invalid){
+      console.log('Todos los campos son requeridos')
+      return;
+    }
+
    //si usuario = admin y password = admin
    //entonces console.log(login correcto)
-   //sinp login incorrectyo
+   //sinp login incorrecto
 
-   if(this.email== 'admin' && this.password=='admin'){
+
+   //ventaja num 1, validaciones, agrupacion de info, etc, etc, mandar errores al usuario,
+   //validar si el formulario se
+
+   if(this.email=== 'admin' && this.password==='admin'){
     console.log('Login correcto')
    }else{
     console.log('Login incorrecto');
